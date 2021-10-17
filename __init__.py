51174a1982e4180a53ea8260a8ba176f8d63a1b5
Alexey <support@uvviewsoft.com>
2021-10-17 12:34:21 +0300

    +

---
+++ __init__.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+from cudatext import *
+
+MYCELL = 110
+
+class Command:
+    id_st = 0
+    inited = False
+
+    def tick(self, tag='', info=''):
+        # add statusbar cell later, so it appears in the right edge
+        if not self.inited:
+            self.inited = True
+            statusbar_proc(self.id_st, STATUSBAR_ADD_CELL, tag=MYCELL)
+            statusbar_proc(self.id_st, STATUSBAR_SET_CELL_AUTOSIZE, tag=MYCELL, value=1)
+
+        now = datetime.now()
+        s = now.strftime("%H:%M:%S")
+        statusbar_proc(self.id_st, STATUSBAR_SET_CELL_TEXT, tag=MYCELL, value=s)
+
+    def on_start(self, ed_self):
+        self.id_st = app_proc(PROC_GET_MAIN_STATUSBAR, '')
+        timer_proc(TIMER_START, 'cuda_clock.tick', 1000, '')
